c3aaf921925bc6c14186f047075c4287a25a59e7
chris <christoperjoel1@gmail.com>
2026-07-07 10:24:24 +0700

    Added more categories to allow heavy cargo, super sonic cruiser, and general aviation

--- src/Model/PlaneCategory.java
+++ src/Model/PlaneCategory.java
@@ -1,9 +1,12 @@
 package Model;
 
 public enum PlaneCategory {
-    REGIONAL(250, 20.0/60.0),
+    GENERAL_AVIATION(100, 15.0/60.0),
+    REGIONAL(250, 18.0/60.0),
     NARROW_BODY(500, 24.0/60.0),
-    WIDE_BODY(1500, 28.0/60.0);
+    WIDE_BODY(3000, 28.0/60.0),
+    HEAVY_CARGO(5000, 28.0/60.0),
+    SPECIAL(4000, 28.0/60.0);
 
     public final double taxiFuel;
     public final double climbHour;
